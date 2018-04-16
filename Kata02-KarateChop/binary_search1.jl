040d30ff7c324d5ab6301d1957c3080e740de9b6
schrist <SimonChrist@gmx.de>
2018-04-16 15:22:01 +0200

    fixed type instability

--- Kata02-KarateChop/binary_search1.jl
+++ Kata02-KarateChop/binary_search1.jl
@@ -5,20 +5,16 @@ println( "\n================= RECURSION: =================" )
 
 Perform a binary search on a sorted `collection` to find `target`. Return -1 if the target hasn't been found. Return the index otherwise.
 """
-function chop( target, collection )
-    offset = 0
-    function _chop( target, collection )
-        l = length( collection )
-        spot = (l + 1) ÷ 2
-        l == 0 && return -1
-        if collection[spot] == target
-            return offset + spot
-        elseif collection[spot] > target
-            _chop( target, collection[1:spot-1] )
-        else
-            offset += spot
-            _chop( target, collection[spot+1:end] )
-        end
+function chop( target, collection, offset = 0 )
+    l = length( collection )
+    spot = (l + 1) ÷ 2
+    l == 0 && return -1
+    if collection[spot] == target
+        return offset + spot
+    elseif collection[spot] > target
+        chop( target, collection[1:spot-1], offset )
+    else
+        offset += spot
+        chop( target, collection[spot+1:end], offset )
     end
-    return _chop( target, collection )
 end
